7da3d2af4a903565ceb12908e8dcf6e296d8a2e3
Tri <trivu2021@gmail.com>
2024-10-13 23:43:55 -0500

    Refactor
    Add character purple prefab
    Combine Player_Movement, Player_Health, and Player_Attack into Player_Manager

---
+++ Assets/Scripts/Player/Player_Manager.cs
@@ -0,0 +1,134 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Manager : MonoBehaviour
+{
+    [SerializeField] private Player_Stat _stat;
+    [SerializeField] private Animator _anim;
+
+    private Rigidbody2D _rb;
+    private Transform _transform;
+
+    private float _moveInput;
+    private bool _isOnGround;
+    private bool _isSecondJump;
+    private float _groundCheckDistance = 1f;
+
+    private int _health;
+
+    private float _attackCoolDown;
+
+
+    /* Monobehavior methods */
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _transform = transform;
+        _health = _stat._maxHealth;
+    }
+    private void Update()
+    {
+        _attackCoolDown += Time.deltaTime;
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GroundCheck();
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, Vector2.down * _groundCheckDistance); // Ground check ray
+    }
+
+    /* Movement handlers */
+    private void Move()
+    {
+        //_rb.velocity += Vector2.right * _moveInput * _stat._moveSpeed * Time.fixedDeltaTime;
+        float xSpeed = Mathf.MoveTowards(_rb.velocity.x, _stat._moveSpeed * _moveInput, _stat._accelerationSpeed * Time.fixedDeltaTime);
+        _rb.velocity = new Vector2(xSpeed, _rb.velocity.y);
+    }
+    private void Jump()
+    {
+        if (_isOnGround || !_isSecondJump)
+        {
+            if (!_isOnGround) 
+                _isSecondJump = true;
+
+            _rb.velocity = _rb.velocity - _rb.velocity.y * Vector2.up;  // Reset y velocity
+            _rb.AddForce(Vector2.up * _stat._jumpForce, ForceMode2D.Impulse);
+            _anim.SetTrigger("jump");
+            _anim.SetBool("onGround", false);
+            _isOnGround = false;
+        }
+    }
+    private void GroundCheck()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(_transform.position, Vector2.down * _groundCheckDistance);
+        if (hit.collider != null)
+        {
+            _isOnGround = true;
+            _isSecondJump = false;
+            _anim.SetBool("onGround", true);
+        }
+    }
+
+
+    /* Health Handlers */
+    public void TakeDamage(int value)
+    {
+        _health -= value;
+        _anim.SetTrigger("hurt");
+
+        /* Implement hurt mechanic here */
+
+
+        if (_health <= 0) Die();
+    }
+    private void Die()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+    /* Attack Handlers */
+    private void AttackAnimation()
+    {
+        int randomAttackAnimation = Random.Range(0, 2);
+
+        if (_moveInput == 0)
+            _anim.SetTrigger(randomAttackAnimation == 1 ? "attack1" : "attack2");                    // Attack while standing still
+        else
+            _anim.SetTrigger(randomAttackAnimation == 1 ? "walkAttack1" : "walkAttack2");   // Attack while walking
+    }
+    private void Attack()
+    { 
+        /* Implement attack mechanic here */
+
+    }
+
+
+    /* Input Handlers => New Input System */
+    private void OnMove(InputValue value)  
+    {
+        _moveInput = value.Get<Vector2>().x;
+        _anim.SetInteger("moveInput", (int)_moveInput);
+        if (_moveInput != 0) _transform.localScale = new Vector3((int)_moveInput, 1, 1);    // Flip player to direction
+    }
+    private void OnJump()
+    {
+        Jump();
+    }
+    private void OnAttack()
+    {
+        if (_attackCoolDown >= _stat._attackCooldown)
+        {
+            _attackCoolDown = 0;
+            Attack();
+            AttackAnimation();
+        }
+    }
+}
--- Assets/Scripts/Player/Player_Stat.cs
+++ Assets/Scripts/Player/Player_Stat.cs
@@ -7,10 +7,16 @@ public class Player_Stat : ScriptableObject
 {
     [SerializeField] private int maxHealth;
     public int _maxHealth { get => maxHealth; private set { maxHealth = value; } }
+
     [SerializeField] private float moveSpeed;
     public float _moveSpeed { get => moveSpeed; private set {moveSpeed = value; } }
+
     [SerializeField] private float jumpForce;
     public float _jumpForce { get => jumpForce; private set { jumpForce = value; } }
+
     [SerializeField] private float attackCooldown;
     public float _attackCooldown { get => attackCooldown; private set { attackCooldown = value; } }
+
+    [SerializeField] private float accelerationSpeed;
+    public float _accelerationSpeed { get => accelerationSpeed; private set { accelerationSpeed = value; } }
 }
